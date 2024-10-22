5aa66201f6f425a608b71c6b8756e6d3f9149497
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-22 22:31:02 +0900

    page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,30 @@
-<main>
-  <h1>Main</h1>
-</main>
+<script>
+    import { goto } from '$app/navigation';
+    
+    function navigateToShort() {
+        goto('/short');
+    }
+</script>
+
+<style>
+  button {
+    padding: 10px 20px;
+    font-size: 1rem;
+    border: none;
+    border-radius: 10px;
+    background-color: rgba(0, 123, 255, 0.1);
+    color: #fff;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    backdrop-filter: blur(10px);
+    animation: fadeIn 1s ease-in-out;
+  }
+  
+  button:hover {
+    background-color: rgba(0, 86, 179, 0.1);
+  }
+</style>
+
+<button on:click={navigateToShort}>
+    짧은글연습
+</button>
