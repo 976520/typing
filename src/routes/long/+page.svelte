ec87e2892b0c19dbe98136c26f005722d4bfaf3c
송재욱 <haensol976520@gmail.com>
2024-11-05 09:14:31 +0900

    gohome

--- src/routes/long/+page.svelte
+++ src/routes/long/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-
+	import { goto } from '$app/navigation';
 	let words: string[] = [];
 	let currentInput: string = '';
 	let currentWordIndex: number = 0;
